e9de5e267066683355d6db1b7e2c446dc11b2057
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-08-21 14:36:56 +0800

    update theme

--- app.js
+++ app.js
@@ -24,6 +24,7 @@
   try {
     sails = require('sails');
   } catch (e) {
+    console.log(e)
     console.error('To run an app using `node app.js`, you usually need to have a version of `sails` installed in the same directory as your app.');
     console.error('To do that, run `npm install sails`');
     console.error('');
