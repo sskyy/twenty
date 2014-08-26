1f6d94c45289d1530c875f801ac99a97f3d7d4d6
sskyy <skyking_H@hotmail.com>
2014-08-26 11:21:38 +0800

    update brief function

--- api/hooks/node/index.js
+++ api/hooks/node/index.js
@@ -1,25 +1,32 @@
+var htmlToText = require('html-to-text');
+
 module.exports  =  function( sails){
   var nodeModels = Object.keys(sails.models).filter( function( name ){
     return sails.models[name].isNode
   })
 
+
+  function brief( req, res, next ){
+    if( req.param(config.field).length > config.limit + config.overflow ){
+      //TODO 摘要规则进一步优化
+
+      req.body[config.toField] =
+        htmlToText.fromString(req.param(config.field), {
+          wordwrap: config.limit
+        })
+
+    }
+    next()
+  }
+
   var config = sails.config.cms.node.brief
   return config.auto == true ? {
     routes : {
-      before : _.zipObject( nodeModels.map(function( name ){
-        return 'post /'+name
-      }), nodeModels.map( function( name ){
-        return function( req, res, next ){
-          if( req.param(config.field).length > config.limit + config.overflow ){
-            req.body[config.toField] =
-              req.param(config.field)
-              .replace(/\n/g,'')
-                .replace(/<\/?\w+(\s[-=\w'"]+)*\s*\/?>/ig,"")
-                .slice(0,config.limit ) + "..."
-          }
-          next()
-        }
-      }))
+      before : _.reduce( nodeModels,function( a, b){
+        a['post /'+b] = brief
+        a['put /'+b] = brief
+        return a
+      },{})
     }
   } : {}
 }
