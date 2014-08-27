a526ca5d3b9db3f72c65091e41208ab5cdfce1b1
sskyy <skyking_H@hotmail.com>
2014-08-27 12:59:34 +0800

    fix bug for statistics

--- api/hooks/statistic/drivers/page.js
+++ api/hooks/statistic/drivers/page.js
@@ -1,9 +1,10 @@
 module.exports = function( sails ){
   var moment = require('moment'),
-    type = "dailyPageView",
-    key = moment(new Date()).format('YYYY-MM-DD')
+    type = "dailyPageView"
+
 
   function logDailyView( req, res, next ){
+    var key = moment(new Date()).format('YYYY-MM-DD')
     Statistic.findOne({key: key,type:type}).then(function(s){
 
       return s ? Statistic.update({key:key,type:type},{value: s.value+1}): Statistic.create({key:key,value:1,type:type})
